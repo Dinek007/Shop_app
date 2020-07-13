472274ecbd6d7bb96f0e182e26a1a9941a5f8747
dinek <wilk.dinek@gmail.com>
2020-07-13 19:55:38 +0200

    init structure

---
+++ src/store/reducers.js
@@ -0,0 +1,13 @@
+import { handleActions } from 'redux-actions'
+import produce from 'immer'
+
+import { ACTIONS } from './consts'
+
+
+const defaultState = {
+
+}
+
+export const reducer = handleActions({
+
+}, defaultState)
