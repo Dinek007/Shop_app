e620bf080a689fb8cd69ad7b06e6fa7cbba87af4
dinek <wilk.dinek@gmail.com>
2020-07-13 20:24:56 +0200

    add header

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from 'react'
+
 import { FooterContainer } from "./pages/footer/footerContainer"
+import { HeaderContainer } from "./pages/header/headerContainer"
 
 export const App = () => {
     return (
         <div>
             <FooterContainer></FooterContainer>
+            <HeaderContainer></HeaderContainer>
         </div>
     )
 }
---
+++ src/pages/header/headerContainer.js
@@ -0,0 +1,10 @@
+import React from "react"
+
+import { HeaderComponent } from "./headerComponent"
+
+export const HeaderContainer = () => {
+
+    return (
+        <HeaderComponent></HeaderComponent>
+    )
+}
