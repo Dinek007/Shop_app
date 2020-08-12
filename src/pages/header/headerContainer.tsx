56155287ee9a0f31c60fd7d6f4f1ab198330f119
dinek <wilk.dinek@gmail.com>
2020-08-12 16:23:59 +0200

    add containers functions

--- src/pages/header/headerContainer.tsx
+++ src/pages/header/headerContainer.tsx
@@ -1,11 +1,46 @@
 import React from "react"
+import { useSelector, useDispatch } from "react-redux"
 
 import { HeaderComponent } from "./headerComponent"
 import { HeaderContainerProps } from "./types"
+import { ReduxState } from "../../store/reducers"
+import { actions } from "../../store/actions"
+
+const checkboxNames = [
+    "Woman",
+    "Man",
+    "New",
+    "Used",
+    "ExDisplay"
+]
+
+const useCheckBox = (date: any) => {
+    const dispatch = useDispatch()
+    console.log(date)
+    return () => {
+        dispatch(actions.fetchProducts(date))
+    }
+}
+
+const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    console.log(event.target.name, event.target.checked)
+    const date = {
+        name: event.target.name,
+        checked: event.target.checked
+    }
+    const checkboxHook = useCheckBox(date)
+};
 
 export const HeaderContainer: React.FC<HeaderContainerProps> = () => {
-
+    const category = useSelector((store: ReduxState) => store.chosenCategory)
+    const checkboxs = useSelector((store: ReduxState) => store.checkboxNames)
+    // 
     return (
-        <HeaderComponent></HeaderComponent>
+        <HeaderComponent
+            categoryName={category}
+            checkboxNames={checkboxNames}
+            checkboxs={checkboxs}
+            handleChange={handleChange}
+        />
     )
 }
--- src/pages/products/productsContainer.tsx
+++ src/pages/products/productsContainer.tsx
@@ -1,11 +1,13 @@
 import React from "react"
+import { useSelector } from "react-redux"
 
 import { ProductsComponent } from "./productsComponent"
 import { ProductsContainerProps } from "./types"
+import { ReduxState } from "../../store/reducers"
 
 export const ProductsContainer: React.FC<ProductsContainerProps> = () => {
-
+    const products = useSelector((store: ReduxState) => store.products)
     return (
-        <ProductsComponent/>
+        <ProductsComponent {...products} />
     )
 }
