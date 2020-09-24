57ec0a026a3ff99573e7c636cb26ac8a25a38c0e
dinek <wilk.dinek@gmail.com>
2020-09-24 18:13:13 +0200

    new themes

--- src/theme.ts
+++ src/theme.ts
@@ -2,7 +2,11 @@ import { createMuiTheme, responsiveFontSizes } from "@material-ui/core";
 
 export const colors = {
   dark: '#aaaaaa',
-  color: '#3366cc'
+  color: '#00A0C9'
+}
+
+const applyStandardFont = {
+  fontFamily: `'Arima Madurai', sans-serif`
 }
 
 export const theme = responsiveFontSizes(createMuiTheme({
@@ -12,6 +16,31 @@ export const theme = responsiveFontSizes(createMuiTheme({
     },
     secondary: {
       main: colors.color
+    }
+  },
+  overrides: {
+    MuiTypography: {
+      root: {
+        ...applyStandardFont
+      },
+      h4: {
+        ...applyStandardFont
+      },
+      h2: {
+        ...applyStandardFont,
+        fontWeight: 'lighter',
+        fontSize: 25
+      },
+      body1: {
+        ...applyStandardFont,
+        fontSize: 17
+      }
+    },
+    MuiButton: {
+      label: {
+        fontSize: 18,
+
+      }
     }
   }
 }))
