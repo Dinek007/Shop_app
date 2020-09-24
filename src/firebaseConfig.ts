283199257ab9797eaddabf52eaa275053eacdaa9
dinek <wilk.dinek@gmail.com>
2020-09-24 18:13:42 +0200

    firebase config

---
+++ src/firebaseConfig.ts
@@ -0,0 +1,10 @@
+export const config = {
+  apiKey: "AIzaSyDjjtTSzuUqpuyD-BKQVuoM7MrLhEJqGDA",
+  authDomain: "shop-fashion-mistery.firebaseapp.com",
+  databaseURL: "https://shop-fashion-mistery.firebaseio.com",
+  projectId: "shop-fashion-mistery",
+  storageBucket: "shop-fashion-mistery.appspot.com",
+  messagingSenderId: "455354449588",
+  appId: "1:455354449588:web:dc9575da5e4e3b057721e1",
+  measurementId: "G-EKBFDFXXCH"
+}
